a02b8a0f4dbc6f88201fdafea622016c0739bd05
surafelmengist <surafelmengist2015@gmail.com>
2025-06-28 13:25:35 +0300

    age calculator simple function

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const ageCalculator={
-    calculateAge : function(birthYear){
-      const currentYear = new Date().getFullYear();
-      return currentYear - birthYear
-    },
-    displayAge:function (birthYear){
-       const age = this.calculateAge(birthYear);
-       console.log(`i am ${age} years old`)
-
-    }
-
-
+const AgeCalculatorAfter10Years =function (birthYear){
+   const currentYear=new Date().getFullYear();
+   const age=currentYear -birthYear;
+   let ageAfter10 = age+10
+   console.log(`your age after 10 years ago will be ${ageAfter10}`)
 }
-
-ageCalculator.displayAge(1992);
-
-
+AgeCalculatorAfter10Years(1992)
