8612dca5d577604943818bf5a470f3aceb589485
surafelmengist <surafelmengist2015@gmail.com>
2025-06-28 13:39:57 +0300

    while loop is javascript

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const AgeCalculatorAfter10Years =function (birthYear){
-   const currentYear=new Date().getFullYear();
-   const age=currentYear -birthYear;
-   let ageAfter10 = age+10
-   console.log(`your age after 10 years ago will be ${ageAfter10}`)
+// while loop in javascript
+let i=0;
+while(i<10){
+   console.log(i);
+   i++;
 }
-AgeCalculatorAfter10Years(1992)
