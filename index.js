0ac995b5513d119327b447c5b824ec92aa8d86ad
surafelmengist <surafelmengist2015@gmail.com>
2025-06-29 22:00:48 +0300

    random number generator in js

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-function RandomNumber(){
-   let random=Math.floor(Math.random()*10)
-   if(random<=5){
-         console.log("the random number is less than or equal to five " + random)
-   }
-   else{
-      console.log(`the number is greater than five ${random}`)
-   }
+// write javascript programming code to execute grade system  by using if else statement
+
+
+function gradeSystem(){
+    let mark=prompt("Enter your marks : ");
+    mark=parseInt(mark);
+    if(mark>=90 && mark <=100){
+        console.log(`You got ${mark} marks and your grade is A+`);
+    }
+    else  if(mark>=85 && mark <90){
+        console.log(`You got ${mark} marks and your grade is A`);
+    }
+    else if(mark>=80 && mark <=84){
+        console.log(`You got ${mark} marks and your grade is A-`);
+    }
+    else if(mark>=75 && mark <=79){
+        console.log(`You got ${mark} marks and your grade is B+`);
+    }
+   else if(mark>=70 && mark <=74){
+        console.log(`You got ${mark} marks and your grade is B`);
+    }
+      else  if(mark>=65 && mark <=69){
+        console.log(`You got ${mark} marks and your grade is B-`);
+    }
+     else if(mark>=60 && mark <=64){
+        console.log(`You got ${mark} marks and your grade is C+`);
+    }
+     else  if(mark>=55 && mark <= 59){
+        console.log(`You got ${mark} marks and your grade is C`);
+    }
+ else if(mark>=50 && mark <=54){
+            console.log(`You got ${mark} marks and your grade is C-`);
+
+ }
+ else if(mark>=40 && mark <=49){
+            console.log(`you got ${mark} marks and your grade is D`);
+ }
+    else if(mark >=0 && mark <=39){
+         console.log(`you got ${mark} marks and your grade is F`);
+ 
+    }
+    else{
+        console.log(" invalid input please enter the number between 0 to 100");
+    }
+ 
 }
-RandomNumber()
+gradeSystem();
