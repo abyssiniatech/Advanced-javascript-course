1781c02ce967dd87d8c74991969f56ec38bd7cec
surafelmengist <surafelmengist2015@gmail.com>
2025-06-30 11:29:55 +0300

    this keyword is js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-// IIFE
+// this keyword is used to access the global object in node js
+// the first function of this keyword is used to call inside the objects
+// example 1
+const info={
+    name: "john",
+    age:30,
+    greet:function(){
+        console.log(`Hello my name is ${this.name}  and  i 'm ${this.age} years old`)
+    },
 
-(function(){
-    console.log("iife fucntion in js file ")
-})();
+ismale:true
+}
+
+info.greet();
+
+console.log(this.name);
