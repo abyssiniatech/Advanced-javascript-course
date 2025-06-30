dcefccd12d2c8fb1d6936447c5f47d9dd1f581ff
surafelmengist <surafelmengist2015@gmail.com>
2025-06-30 12:39:03 +0300

    the bind() method in js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// this keyword is used to access the global object in node js
-// the first function of this keyword is used to call inside the objects
-// example 1
-const info={
-    name: "john",
-    age:30,
-    greet:function(){
-        console.log(`Hello my name is ${this.name}  and  i 'm ${this.age} years old`)
-    },
-
-ismale:true
+// bind() is a method of the function  object that creates a new function that when called has its this keyword set to the provide value with a given sequence of arguments preceding any provided when the new function is called.accordion
+// example one code bind
+function multiply(a,b){
+    return a*b;
 }
 
-info.greet();
+const multiplyByTwo=multiply.bind(null,2);
+console.log(multiplyByTwo(10));
 
-console.log(this.name);
+// example tw code bind
+const multiplyByfFour=multiply.bind(null,5);
+console.log(multiplyByfFour)
+
+
+
+
+
+
+// the second function to  execute the sum of the two numbers
+const add=(a,b)=>{
+    return a+b;
+}
+const addByTwo = add.bind(null,4);
+console.log(addByTwo(10))
