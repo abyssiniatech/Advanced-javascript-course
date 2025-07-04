2d90c1a28029b4059a95a252f1b67985bc6a35be
surafelmengist <surafelmengist2015@gmail.com>
2025-07-04 12:21:24 +0300

    find the sum of the given array by using for loop and foreach loop

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-let colors=["black","yellow",'red','white'];
-// execute the above code by using for 
-for(let i=0; i<colors.length; i++){
-  console.log(colors[i])
+let numbers=[1,2,3,4,5,6,7];
+var sum=0;
+// calculate the sum of all numbers in the array
+for(let i=0; i<numbers.length;i+=1){
+  sum +=numbers[i];
+  console.log(`The sum of the numbers is  =   ${sum}`)
 }
 
 
-// execute array by using foreach method
-colors.forEach(color=>{
-  console.log(color)
+// calculate the sum of the nunbers in the array using foreach method
+numbers.forEach(num=>{
+  sum +=num;
+ 
 })
+ console.log(`The sum of the numbers using the given  method is = ${sum}`)
