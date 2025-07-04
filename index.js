c02032d63c2821de6f95157f23907cc7c2a21c22
surafelmengist <surafelmengist2015@gmail.com>
2025-07-04 14:36:25 +0300

    class in js

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-let frainds=[
-  {name:"surafel",department: 'computer science'},
-  {name:"abel",department: 'software engineering'},
-  {name:"surfael",department: 'mechanical engineering'},
-  {name:"surfael",department: 'civil engineering'},
-]
-// filtering the array of objects based on the name property
-let filterFrainds = frainds.filter(friend =>frainds.name == "surafel")
-console.log(filterFrainds)
+class Teacher{
+  constructor(name,age){
+      this.name =name;
+      this.age= age;
+  }
 
-
-// 
- frainds.filter((list) =>{
-  
-  console.log(list.department == "computer science");
-  
-      
-}) 
+}
+class Student extends Teacher{
+  constructor(name,age,grade){
+    super(name,age)
+    this.grade= grade;
+  }
+}
 
 
 
-
-// the third example
-let num=[1,2,3,4,5,6,7];
-let final=num.filter(numlist => numlist %2 ===0);
-console.log(`the even number : ${final}`)
+const person1=new Teacher("yared",29)
+const person2=new Student("Abel",12,11)
+console.log(person1)
+console.log(person2)
